1ae9b81aae890c7c124b1b886a7dd0cd5ed9e59e
Rohan <rohanbfg@gmail.com>
2017-08-25 22:16:14 +0530

    pushj

--- project1.py
+++ project1.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 print ("Enter number of variabes") #number of variables the function is dependent on
 numbers = input()
 print ("enter splitting order")
@@ -7,14 +9,18 @@ print("Enter the expression")
 file=open("input.txt","r")        #complements are represented with capital letters
 input1=file.readlines()
 
+
 for i in range(len(input1)) :
 	input1[i]=list(input1[i][:-1])
+print(input1)
+input1 = np.array(input1,dtype='object')
 print(input1)	
-input2=tuple(input1)
+input2=input1.copy()
 print(input2)
+
 for i in range(len(input1)) :
 	for j in range(len(order)):
-		#input2[i][j]='11'
+		input2[i][j]="11"
 		if order[j] in input1[i]:     	
 			input2[i][j]='01'
 		print (ord(order[j]))	
