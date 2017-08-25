3a75c4f413d39b371a9a7f174767b32e54603a0d
rohanbfg <rohanbfg@gmail.com>
2017-08-25 21:14:00 +0530

    push

---
+++ project1.py
@@ -0,0 +1,26 @@
+print ("Enter number of variabes") #number of variables the function is dependent on
+numbers = input()
+print ("enter splitting order")
+order=list(input())	
+print (order)			  
+print("Enter the expression")      
+file=open("input.txt","r")        #complements are represented with capital letters
+input1=file.readlines()
+
+for i in range(len(input1)) :
+	input1[i]=list(input1[i][:-1])
+print(input1)	
+input2=tuple(input1)
+print(input2)
+for i in range(len(input1)) :
+	for j in range(len(order)):
+		#input2[i][j]='11'
+		if order[j] in input1[i]:     	
+			input2[i][j]='01'
+		print (ord(order[j]))	
+		if chr(ord(order[j]) - 32) in input1[i]:
+			input2[i][j]='10'
+		 
+			
+print(input1)
+print (input2)	
