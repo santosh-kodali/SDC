e74e94dd67652399f6b01162d9326b6354cea085
Tarun Mittal <trmittal24@gmail.com>
2017-11-05 13:55:43 +0530

    urp_tauto

--- project3/consensus.py
+++ project3/consensus.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+import collections
 def logical_and (A,B):
 	
 	if A=='00' or B=='00':
@@ -25,6 +25,23 @@ def logical_and (A,B):
 
 	return output1
 
+class Queue:
+    def __init__(self):
+        self.items = []
+
+    def isEmpty(self):
+        return self.items == []
+
+    def enqueue(self, item):
+        self.items.insert(0,item)
+
+    def dequeue(self):
+        return self.items.pop()
+
+    def size(self):
+        return len(self.items)
+    def contents(self):
+    	return self.items
 
 def logical_or (A,B):
 	if A=='01' and B=='01':
@@ -85,20 +102,24 @@ def consensus (list1,list2):
 
 def cofactor (list1,list2):
 	flag=0
-	result=[0 for x in range(len(list1))]
-	print (result)
-	for i in range(len(list1)):
-		if logical_and(list1[i],list2[i])=='00':
-			flag=1
-			print(flag)
-			break
-	if flag==1:
-		return
+	
+	#print (result)
+	if(list1 is None or list2 is None):
+		return None
 	else:
+		result=[0 for x in range(len(list1))]	
 		for i in range(len(list1)):
+			if logical_and(list1[i],list2[i])=='00':
+				flag=1
+				#print(flag)
+				break
+		if flag==1:
+			return
+		else:
+			for i in range(len(list1)):
 			
-			result[i]=logical_or(list1[i],logical_not(list2[i]))
-		return result
+				result[i]=logical_or(list1[i],logical_not(list2[i]))
+			return list(result)
 
 
 def sharp (list1,list2):
@@ -140,8 +161,68 @@ def disjoint_sharp(list1,list2):
 
 
 
+def select_tautology(filepath):
+	file=open(filepath,"r")
+	data=file.readlines()
+	dict={}
+	q=Queue()
+
+	for i in range(len(data)):
+		data[i]=data[i][:-1]
+	input1 = [[0 for y in range(int(len(data[0])/2))] for x in range(len(data))]		
+	
+	for i in range(len(data)):	
+		input1[i]=[data[i][j:j+2] for j in range(0, len(data[i]), 2)]	
+	
+	for j in range(len(input1[0])):
+		count=0
+		for i in range(len(input1)):
+			if input1[i][j]!='11':
+				count+=1
+		dict[j]=count	
+	#print(dict)	
+	sortedC = collections.OrderedDict(sorted(dict.items(), key=lambda x:x[1], reverse=True))
+	#print(sortedC)
+	order=list(sortedC.keys())
+	#print(order)
+	list2=list(input1)
+	list3=list(input1)
+	q.enqueue(input1)
+	for j in range(len(order)):
+		list1=list(q.dequeue())
+		
+		print(list1)
+		k1 =['11' for y in range(len(input1[j]))]
+		k2 =['11' for y in range(len(input1[j]))]
+		k1[order[j]]='01'
+		k2[order[j]]='10'
+		
+		print(k1)
+		print(k2)
+		for i in range(len(input1)):
+			list2[i]=(cofactor(list1[i],k1))
+			
+			#	flag1=1
+			#	break
+			list3[i]=(cofactor(list1[i],k2))
+			#if list3[i]==['11','11','11']:
+			#	flag2=1
+			#	break
+
+		#if flag1==0:	
+		k3 =['11' for y in range(len(input1[j]))]
+
+		if not(k3 in list(list2)):
+			q.enqueue(list(list2))
+		if not(k3 in list(list3)):
+			q.enqueue(list(list3))
 
 
+		print(q.contents())
+	if(q.isEmpty()):
+		return 1
+	else :
+		return 0
 
 
 
@@ -149,6 +230,9 @@ def disjoint_sharp(list1,list2):
 list1=['11','11']
 list2=['01','01']
 
+#ans=select_tautology("input.txt")
+#print(ans)
 consensus=disjoint_sharp(list1,list2)
+
 print (consensus)
 #print(cofactor(list1,list2))
